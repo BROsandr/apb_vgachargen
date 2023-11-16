63367181811a5db406f05fa83ee8ce92233168ef
alexkharl <sasha_xarlamov@mail.ru>
2023-11-16 19:03:32 +0300

    Feat: init

---
+++ rtl/VGA_TextMode_topModule.v
@@ -0,0 +1,104 @@
+`timescale 1ns / 1ps
+
+
+module VGA_TextMode_topModule
+                (
+                    input wire clk,
+                    input wire rst,
+                    
+                    output wire [3:0]R, 
+                    output wire [3:0]G, 
+                    output wire [3:0]B,
+
+                    output wire hSYNC,
+                    output wire vSYNC
+                );
+                
+                
+                
+localparam CHARACTER_SET_COUNT = 256;
+localparam CHARACTER_ROM_MEMLOC = "CharacterROM_ASCII.mem";
+localparam CHARACTER_BUFFER_MEMLOC = "characterBuffer80x60.mem";
+
+wire [$clog2(640)-1:0]xPixel;
+wire [$clog2(480)-1:0]yPixel;
+wire pixelDrawing;
+
+VGA_Block
+                #
+                (
+                    .MODES(0) // finixing to 640 x 480 for 80 x60 text buffer
+                )
+                VGABLOCKIns
+                (
+                    .systemClk_125MHz(clk),
+                    .rst(rst),
+
+                    .xPixel(xPixel),
+                    .yPixel(yPixel),
+                    .pixelDrawing(pixelDrawing),
+
+                    .hSYNC(hSYNC),
+                    .vSYNC(vSYNC)
+                );
+
+
+wire [$clog2(80*60)-1:0]currentCharacterPixelIndex;
+wire [$clog2(64)-1:0]characterXY;
+
+TextMode_indexGenerator TMindexGenIns
+                (
+                    .xPixel(xPixel),
+                    .yPixel(yPixel),
+
+                    .currentCharacterPixelIndex(currentCharacterPixelIndex),
+                    .characterXY(characterXY)
+                );
+
+
+
+wire [$clog2(CHARACTER_SET_COUNT)-1:0]currentCharacterIndex;
+
+TextMode_textBuffer80x60
+                #
+                (
+                    .CHARACTER_SET_COUNT(CHARACTER_SET_COUNT),
+                    .MEMFILELOC(CHARACTER_BUFFER_MEMLOC)
+                )
+                TMtextBuffIns
+                (
+                    .clk(clk),
+                    .enable(1),
+                    .write_enable(0),
+                    .inputData(0),
+
+                    .currentCharacterPixelIndex_addressIn(currentCharacterPixelIndex),
+
+                    .currentCharacterIndex_dataOut(currentCharacterIndex)
+                );
+
+wire [64-1:0]currentCharacter;
+
+TextMode_characterROM
+                #
+                (
+                    .CHARACTER_SET_COUNT(CHARACTER_SET_COUNT),
+                    .MEMFILELOC(CHARACTER_ROM_MEMLOC)
+                )
+                TMcharacterROMIns
+                (
+                    .clk(clk),
+                    .enable(1),
+
+                    .chracterIndex_addressIn(currentCharacterIndex),
+                    .currentCharacter_dataOut(currentCharacter)
+                );
+
+wire   currentPixel;
+assign currentPixel = (pixelDrawing == 1) ? ~currentCharacter[characterXY] : 0;
+
+assign R = (currentPixel) ? 4'b1: 4'b0;
+assign B = (currentPixel) ? 4'b1: 4'b0;
+assign G = (currentPixel) ? 4'b1: 4'b0;
+
+endmodule
---
+++ rtl/wrapper.sv
@@ -0,0 +1,41 @@
+module wrapper (
+  input  logic clk_i,
+  input  logic rst_ni,
+
+  output wire [3:0]R_o,
+  output wire [3:0]G_o,
+  output wire [3:0]B_o,
+
+  output wire hSYNC_o,
+  output wire vSYNC_o
+);
+
+  logic clk_125m;
+  logic locked;
+
+  clk_wiz_0 clk_wiz_0 (
+    // Clock out ports
+    .clk_out1(clk_125m),     // output clk_out1
+    // Status and control signals
+    .resetn(rst_ni), // input resetn
+    .locked(locked),       // output locked
+   // Clock in ports
+    .clk_in1(clk_i)
+  );      // input clk_in1
+  
+  logic rst;
+
+  assign rst = ~rst_ni | ~locked;
+
+  VGA_TextMode_topModule top (
+    .clk   (clk_125m),
+    .rst,
+
+    .R     (R_o),
+    .G     (G_o),
+    .B     (B_o),
+
+    .hSYNC (hSYNC_o),
+    .vSYNC (vSYNC_o)
+  );
+endmodule
