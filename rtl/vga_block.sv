0c776aacaae02bbcd6925ef9d280e6bce7f19a3e
alexkharl <sasha_xarlamov@mail.ru>
2023-11-22 21:42:52 +0300

    Revert "Fix:Замена timing_gen на tg из школы"
    This reverts commit b37c7126fa20c14d690e92b68411a194309630f3.

--- rtl/vga_block.sv
+++ rtl/vga_block.sv
@@ -21,27 +21,15 @@ module vga_block
     .strb_o  (clk_divider_strb)
   );
 
-  // timing_generator timing_generator (
-  //   .clk_i,
-  //   .arstn_i,
-  //   .en_i     (clk_divider_strb),
-  //   .vga_hs_o,
-  //   .vga_vs_o,
-  //   .hcount_o,
-  //   .vcount_o,
-  //   .pixel_enable_o
+  timing_generator timing_generator (
+    .clk_i,
+    .arstn_i,
+    .en_i     (clk_divider_strb),
+    .vga_hs_o,
+    .vga_vs_o,
+    .hcount_o,
+    .vcount_o,
+    .pixel_enable_o
 
-  // );
-
-  timing_generator #(
-    .CLK_MHZ (100)
-  ) timing_generator (
-    .clk  (clk_i),
-    .rst  (~arstn_i),
-    .hsync(vga_hs_o),
-    .vsync(vga_vs_o),
-    .display_on (pixel_enable_o),
-    .hpos  (hcount_o),
-    .vpos  (vcount_o)
   );
 endmodule
