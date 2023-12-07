d3946994bb1780b6d566d761a128fc10dc3e21f3
alexkharl <sasha_xarlamov@mail.ru>
2023-12-07 11:13:22 +0300

    Feat(vgachargen_pkg):Доб-ие цветов

--- rtl/vgachargen_pkg.sv
+++ rtl/vgachargen_pkg.sv
@@ -32,22 +32,47 @@ package vgachargen_pkg;
   parameter int unsigned CH_MAP_DATA_WIDTH  = CH_T_ADDR_WIDTH + 1;
   parameter int unsigned COL_MAP_ADDR_WIDTH = CH_MAP_ADDR_WIDTH;
 
-  typedef enum bit [11:0] {
-    BLACK = '0,
-    WHITE = '1,
-    BLUE  = 12'h00f,
-    RED   = 12'hf00,
-    GREEN = 12'h0f0
-  } color_t;
+  typedef enum logic [23:0] {
+    COL_0  = 24'h000000,
+    COL_2  = 24'h0000d8,
+    COL_3  = 24'h0000ff,
+    COL_4  = 24'hd80000,
+    COL_5  = 24'hff0000,
+    COL_6  = 24'hd800d8,
+    COL_7  = 24'hff00ff,
+    COL_8  = 24'h00d800,
+    COL_9  = 24'h00ff00,
+    COL_10 = 24'h00d8d8,
+    COL_11 = 24'h00ffff,
+    COL_12 = 24'hd8d800,
+    COL_13 = 24'hffff00,
+    COL_14 = 24'hd8d8d8,
+    COL_15 = 24'hffffff
+  } rgb_t;
 
-  function automatic color_t color_decode(logic [3:0] color_encoded);
-    case (color_encoded)
-      4'h0   : return BLACK;
-      4'ha   : return BLUE;
-      4'hb   : return RED;
-      4'h1   : return GREEN;
-      4'hf   : return WHITE;
-      default: return BLACK;
+  function automatic logic [11:0] rgb2half(rgb_t rgb_i);
+    return {rgb_i[23:20], rgb_i[15:12], rgb_i[7:4]};
+  endfunction
+
+  function automatic logic [11:0] color_decode(logic [3:0] color_encoded_i);
+    unique case (color_encoded_i)
+      4'h0   : return rgb2half(COL_0 );
+      4'h1   : return rgb2half(COL_0 );
+      4'h2   : return rgb2half(COL_2 );
+      4'h3   : return rgb2half(COL_3 );
+      4'h4   : return rgb2half(COL_4 );
+      4'h5   : return rgb2half(COL_5 );
+      4'h6   : return rgb2half(COL_6 );
+      4'h7   : return rgb2half(COL_7 );
+      4'h8   : return rgb2half(COL_8 );
+      4'h9   : return rgb2half(COL_9 );
+      4'ha   : return rgb2half(COL_10);
+      4'hb   : return rgb2half(COL_11);
+      4'hc   : return rgb2half(COL_12);
+      4'hd   : return rgb2half(COL_13);
+      4'he   : return rgb2half(COL_14);
+      4'hf   : return rgb2half(COL_15);
+      default: return rgb2half(COL_0 );
     endcase
   endfunction
 endpackage
